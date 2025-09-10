4b03fcf6327f61ac5f73afa8766871c017796617
igortestat <igorsiv1405@gmail.com>
2025-09-10 16:28:25 +0300

    изменение логики: валидация пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int validFilePathCount = 0; // Счетчик для верно указанных путей к файлам
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            if (path.isEmpty()) {
+                System.out.println("Путь не может быть пустым.");
+                continue;
+            }
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println("Файл не существует.");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указанный путь является директорией, а не файлом.");
+                continue;
+            }
+            validFilePathCount++;
+            System.out.println("Путь указан верно ");
+            System.out.println("Общее количество верно указанных путей к файлам: " + validFilePathCount);
+        }
     }
 }
